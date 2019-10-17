42d30cd85c51868e2aefe89845511d6bec371f55
Kelvin Pompey <kelvin.pompey@gmail.com>
2019-10-17 10:02:22 -0400

    Downgrade alamofire dependency

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
       .package(url: "https://github.com/apollographql/apollo-ios.git", .branch("master")),
-      .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.2")
+      .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.8.2")
     ],
     targets: [
         .target(
